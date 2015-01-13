e365cd78275f8f35a47a5c93415046cb51798900
Matthijs Kamstra aka [mck] <matthijs@fonkmobile.nl>
2015-01-13 12:08:26 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,123 @@
 # go
+Lightweight tween library for Haxe.
+ 
+Inspired by tween engines written in Haxe and ActionScript. I just wanted to see if I could build a tweening engine myself.
+
+####WIP and not ready for production
+
+
+## Installation
+
+Because this is an experiment, there is no haxelib install.
+
+But if you want to try it out via *haxelib*, you can install it with git 
+
+```
+git clone https://github.com/MatthijsKamstra/go/
+haxelib dev go go
+```
+
+To include Go in an OpenFL project, add `<haxelib name="go" />` to your project.xml.
+
+To add Go to a standard Haxe project, use `-lib go` in your HXML
+
+
+
+
+
+## Usage
+
+Animate a sprite in 1.5 second to x postion 100
+
+```
+Go.to(sprite, 1.5).x(100);
+```
+
+Animate a sprite in 1.5 second to x and y postion
+
+```
+Go.to(sprite, 1.5).x(100).y(200);
+```
+
+Animate a sprite in 1.5 second to x and y postion and call function when animation is done
+
+```
+Go.to(sprite, 1.5).x(100).y(200).onComplete(onCompleteHandler, ['hello']);
+```
+
+Rotate an object in degrees
+
+```
+Go.to(sprite, 1.5).rotate(360);
+```
+
+Change the alpha of an object (value from 0 to 1)
+
+```
+Go.to(sprite, 1.5).alpha(.5);
+```
+
+
+## Concept
+
+It should be easy to use, so I decided to use chainging methode.
+
+You can animate 
+
+**to** 
+
+`Go.to(target, 1.5).x(100);`
+
+and 
+
+**from** 
+
+`Go.from(target, 1.5).x(100);`
+
+
+You can animate the default **properties** of a DisplayObject:
+
+* x
+* y
+* rotation 
+* alpha
+* scale
+
+`Go.from(target, 1.5).x(100).y(100).rotation(10).alpha(.5).scale(2);`
+
+and value via **prop**
+
+`Go.from(obj, 1.5).prop('counter',100)`
+
+
+Call an **onComplete** when the animation id done
+
+Use simple easing 
+
+* 	linear
+* 	quad
+* 	cubic
+* 	quart
+* 	quint
+* 	sine
+* 	circ
+* 	expo
+* 	back
+* 	bounce
+* 	elastic
+* 	reverse
+* 	reflect
+
+## Tween engines I used for inspiration
+
+Haxe
+
+* [https://github.com/openfl/actuate](https://github.com/openfl/actuate)
+* https://github.com/alijaya/teew/tree/master/teew
+* https://github.com/sreenaths/twee-haxe
+
+AS3
+
+* https://code.google.com/p/eaze-tween/
+* https://code.google.com/p/fatlib/source/browse/trunk/src/org/fatlib/utils/Tween.as
+
---
+++ lets/Go.hx
@@ -0,0 +1,256 @@
+
+package lets;
+
+import lets.Easing;
+
+import haxe.Timer;
+import flash.Lib;
+
+class Go 
+{
+	private static var _trigger:Timer;
+	private static var _tweens:Array<Go> = new Array<Go>();
+
+	private var _easing:Float->Float;
+	private var _target:Dynamic;
+	private var _duration:Int;
+
+	private var _props = new Map<String,Range>();
+
+	private var _options:Dynamic;
+	private var _isFrom:Bool = false;
+	private var _isComplete:Bool = false;
+
+	private var _initTime:Int = 0;
+
+	/**
+	 * Animate an object to another state (like position, scale, rotation, alpha)
+	 * 
+	 * @example		lets.Go.to(foobarMc, 1.5);
+	 * 
+	 * @param  target   	object to animate
+	 * @param  duration 	in seconds	
+	 * @return          Go
+	 */
+	public function new(target:Dynamic, duration:Float)
+	{
+		this._target = target;
+		this._duration = Std.int (duration * 1000);
+		this._easing = Easing.linear;
+		this._options = cast{};
+		this._initTime = Lib.getTimer();
+		_tweens.push(this);
+		// trace('New Go - _target: ' + _target + ' / _duration: ' + _duration+  ' / _initTime: ' + _initTime+ ' / _tweens.length: ' + _tweens.length);
+		// [mck] extreme little delay to make sure all the values are set
+		haxe.Timer.delay(init, 1); // 1 milisecond delay
+	}
+
+	/**
+	 * Animate an object TO another state (like position, scale, rotation, alpha)
+	 * 
+	 * @example		lets.Go.to(foobarMc, 1.5);
+	 * 
+	 * @param  target   	object to animate
+	 * @param  duration 	in seconds	
+	 * @return          Go
+	 */
+	static inline public function to(target:Dynamic, duration:Float):Go
+	{
+		var go = new Go(target, duration);
+		go._isFrom = false;
+		return go;
+	}
+
+	/**
+	 * Animate an object FROM another state (like position, scale, rotation, alpha)
+	 * 
+	 * @example		lets.Go.from(foobarMc, 1.5);
+	 * 
+	 * @param  target   	object to animate
+	 * @param  duration 	in seconds	
+	 * @return          Go
+	 */	
+	static inline public function from(target:Dynamic, duration:Float):Go
+	{
+		var go = new Go(target, duration);
+		go._isFrom = true;
+		return go;
+	}
+
+	// ____________________________________ properties ____________________________________
+
+	/**
+	 * change the x value of an object
+	 * 
+	 * @param  value 	x-position
+	 * @return       Go
+	 */
+	inline public function x(value:Float):Go
+	{
+		prop ('x', value);
+		return this;
+	}
+	/**
+	 * change the y value of an object
+	 * 
+	 * @param  value 	y-position
+	 * @return       Go
+	 */
+	inline public function y(value:Float):Go
+	{
+		prop ('y', value);
+		return this;
+	}
+	/**
+	 * change the rotation value of an object
+	 * 
+	 * @param  value 	rotation in degrees (360)
+	 * @return       Go
+	 */
+	inline public function rotation(value:Float):Go
+	{
+		prop ('rotation', value);
+		return this;
+	}
+	/**
+	 * change the alpha value of an object
+	 * 
+	 * @param  value 	transparanty value (maximum value 1)
+	 * @return       Go
+	 */
+	inline public function alpha(value:Float):Go
+	{
+		prop ('alpha', value);
+		return this;
+	}
+	/**
+	 * change the scale of an object
+	 * 
+	 * @param  value 	scale (1 is 100% (original scale), 0.5 is 50%, 2 is 200%)
+	 * @return       Go
+	 */
+	inline public function scale(value:Float):Go
+	{
+		prop ('scaleX', value);
+		prop ('scaleY', value);
+		return this;
+	}
+	/**
+	 * change the property of an object
+	 * 
+	 * @param  key   	description of the property as string
+	 * @param  value 	change to this value 
+	 * @return       Go
+	 */
+	inline public function prop(key:String, value:Float):Go 
+	{
+		var objValue = Reflect.getProperty(_target, key);
+		var _range = {key:key, from:(_isFrom) ? value:objValue , to:(!_isFrom) ? value:objValue };
+		_props.set (key, _range );
+		return this;
+	}
+	/**
+	 * on completion of the animation call a function with 
+	 * 
+	 * @param  func         	function to call when animition is complete
+	 * @param  arr<Dynamic> 	params send to function
+	 * @return              Go
+	 */
+	inline public function onComplete(func:Dynamic, arr:Array<Dynamic>):Go
+	{
+		_options.onComplete = func;
+		_options.onCompleteParams = arr;
+		return this;
+	}
+	/**
+	 * change the default (Easing.linear) easing 
+	 *
+	 * @example		lets.Go.from(foobarMc, 1.5).x(500).easing(lets.Easing.quad);
+	 * 
+	 * @param  easing->Float 		check Easing class
+	 * @return		Go
+	 */
+	inline public function ease(easing:Float->Float):Go
+	{
+		this._easing = easing;
+		return this;
+	}
+	
+	// ____________________________________ private ____________________________________
+
+	private function init():Void
+	{
+		var framerate:Int = 30;
+		_trigger = (_trigger == null) ? new Timer(Std.int(1000 / framerate)) : _trigger;
+		_trigger.run = onEnterFrameHandler;
+	}
+
+	private function onEnterFrameHandler( ):Void
+	{
+		if(_initTime == 0) return;
+		if (_tweens.length <= 0)
+		{
+			// [mck] stop timer, we are done!
+			_trigger.stop();
+			_trigger.run = null;
+		} 
+		else for( i in 0..._tweens.length ) 
+		{
+			// [mck] FIXME :: don't know exactly why I need to check if _tweens[i] != null, but I do. 
+			if(_tweens[i] != null) _tweens[i].update();
+		}
+	}
+	
+	private function update():Void
+	{
+		var progressed = Lib.getTimer() - _initTime;
+		if( progressed >= _duration){
+			// [mck] setProperties in the final state
+			updateProperties(_duration);
+			complete();
+		} else {
+			updateProperties(progressed);
+		}
+	}
+
+	private function updateProperties(time:Int):Void 
+	{
+		for(n in _props.keys())
+		{
+			var range = _props.get(n);
+			Reflect.setProperty(_target, n , _easing(time/_duration) * (range.to-range.from) + range.from);
+		}
+		// else throw( "Property "+propertyName+" not found in target!" );
+	}
+
+
+	private function complete():Void 
+	{
+		//trace('complete :: _duration: ' + _duration + ' / _initTime: ' + _initTime + ' / _tweens.length: ' + _tweens.length);
+	
+		var func = _options.onComplete;
+		var arr = _options.onCompleteParams;
+
+		destroy();
+
+		if( Reflect.isFunction(func) ) func.apply( null, arr );
+	}
+
+	private function destroy():Void
+	{
+		if(Lambda.has(_tweens, this)) _tweens.remove(this);
+		// [mck] cleaning up
+		if( _options )
+		{
+			_options = null;
+			_easing = null;
+			_target = null;
+			_props = null;
+			_duration = 0;
+			_initTime = 0;
+		}
+	}
+
+}
+
+typedef Range = {key:String, from:Null<Float>, to:Null<Float>}
